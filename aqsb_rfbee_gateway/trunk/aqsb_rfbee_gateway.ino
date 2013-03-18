9299b55633caa685d13a4d138af8dfcdcc95223e
DustinDemuth <DustinDemuth@9b2695e2-e135-4693-8c03-170fd80ef316>
2013-03-18 11:50:19 +0000

    gitignore
    git-svn-id: https://svn.52north.org/svn/swe/incubation/sensebox/AirQualitySenseBox@19561 9b2695e2-e135-4693-8c03-170fd80ef316

--- aqsb_rfbee_gateway/trunk/aqsb_rfbee_gateway.ino
+++ aqsb_rfbee_gateway/trunk/aqsb_rfbee_gateway.ino
@@ -1,6 +1,14 @@
 #include <EEPROM.h>
-#include <RFBeeSendRev.h>
-#include <RFBeeCore.h>
+#include <../libraries/RFBee/RFBeeSendRev.h>
+#include <../libraries/RFBee/RFBeeCore.h>
+
+#include <avr/wdt.h>
+volatile boolean f_wdt=1;
+// Watchdog Interrupt Service / is executed when  watchdog timed out
+ISR(WDT_vect) {
+  f_wdt=1;  // set global flag
+}
+
 
 unsigned char dtaUart[100];
 unsigned char dtaUartLen = 0;
@@ -9,11 +17,11 @@ bool stringComplete      = false;        // if get data
 #define MYADDRESS 2 //all receivers have ID 2, this doesn't matter anyway as we are receiving broadcasts...
 void setup(){
     RFBEE.init(MYADDRESS, 2);//Modified! This ID, AdressCheck 1=true 2=True+Broadcast
-    Serial.begin(38400);
-    Serial.println("ok");
+    Serial.begin(9600);
+    //Serial.println("ok");
 }
 
-unsigned char rxData1[200];               // data len
+unsigned char rxData1[64];               // data len
 unsigned char len1;                       // len
 unsigned char srcAddress1;
 unsigned char destAddress1;
@@ -23,29 +31,21 @@ int result1;
 
 void loop()
 {
+  wdt_enable(WDTO_4S); //enable Watchdog. The System has 4 Seconds to complete the loop. If this can't be achieved, the device restarts....
+  if (f_wdt == 1) f_wdt = 0;
+  
     if(RFBEE.isDta())
     {
         result1=receiveData(rxData1, &len1, &srcAddress1, &destAddress1, (unsigned char *)&rssi1 , &lqi1);
         for(int i = 0; i< len1; i++)
         {
-            Serial.write(rxData1[i]);
+            if (rxData1[i] != '\r' || rxData1[i] != '\n')Serial.write(rxData1[i]);
         }
     }
-    
-    if (dtaUartLen) {
+  wdt_disable(); //disable watchdog....
 
-        RFBEE.sendDta(dtaUartLen, dtaUart);
-        dtaUartLen = 0;
-    }
 }
 
-void serialEvent() {
-
-    while (Serial.available()) 
-    {
-        dtaUart[dtaUartLen++] = (unsigned char)Serial.read();        
-    }
-}
 
 /*********************************************************************************************************
   END FILE
