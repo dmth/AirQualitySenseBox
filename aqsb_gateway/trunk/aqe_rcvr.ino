00d41a38103324252d13eb21bef04622f63a5f64
DustinDemuth <DustinDemuth@9b2695e2-e135-4693-8c03-170fd80ef316>
2013-03-18 11:53:41 +0000

    Splitting the message
    git-svn-id: https://svn.52north.org/svn/swe/incubation/sensebox/AirQualitySenseBox@19568 9b2695e2-e135-4693-8c03-170fd80ef316

--- aqsb_gateway/trunk/aqe_rcvr.ino
+++ aqsb_gateway/trunk/aqe_rcvr.ino
@@ -3,14 +3,15 @@
 //#include <RFBeeCore.h>
 //#include <SD.h>
 
-#define BEEID 2 //all recivers have ID 2, this doesn't matter anyway as we are receiving broadcasts...
+#include <avr/wdt.h>
+
 #define DTALEN 55 //Length of a message
 
 struct message{
   char mac[13];
-  long lat, lon;
+  float lat, lon;
   unsigned long co, no2;
-  short int humidity, temperature;
+  short int hum, tem;
 };
 
 void setup(){
@@ -22,6 +23,7 @@ void setup(){
 
 
 void loop(){
+  
   boolean newdata = false; //If true, a new message has been received
   
   unsigned int c;
@@ -41,20 +43,69 @@ void loop(){
   12          |1|    9    | 9       |4   |  4 | 8      | 8      |  = 55
   MAC         |:|  LAT    | LON     |HUM |TEM | NO2    | CO     |
   ------------|:|---------|---------|----|----|--------|--------|
+  000000000011|1|111111122|222222223|3333|3333|34444444|44455555|
+  012345678901|2|345678901|234567890|1234|5678|90123456|78901234|
   */
   int result;
-  struct message s_msg;
   if (newdata){
-    result = splitMessage(c_msg, DTALEN, s_msg);    
+    struct message s_msg;
+    s_msg = splitMessage(c_msg);
+    Serial.print(s_msg.mac);
+    Serial.print("  lat: ");
+    Serial.print(s_msg.lat, 5);
+    Serial.print("  lon: ");
+    Serial.println(s_msg.lon, 5);
   }
-  free(c_msg); 
+  
 }
 
-int splitMessage(char* c_msg, int msglength, struct message s_msg){
+/*
+  Split the Message.
+ */
+struct message splitMessage(String c_msg){
+  /*
   for (int i = 0; i < msglength; i++){
    Serial.write(c_msg[i]); 
-  }
-  Serial.println();
+  }*/
+  struct message s_msg;
+  
+  //Serial.println(c_msg);
+  
+  char buf[13];
+  
+  c_msg.substring(0,12).toCharArray(buf, 13);
+  memcpy(s_msg.mac, buf, strlen(buf)+1);
+  //Serial.print(s_msg.mac);
+  //Serial.print("-");
 
-  return 0; //Everything OK
+  c_msg.substring(13,22).toCharArray(buf, 13);
+  s_msg.lat = float(atol(buf))/100000;
+  //Serial.print(s_msg.lat);
+  //Serial.print("-");
+  
+  c_msg.substring(22,31).toCharArray(buf, 13);
+  s_msg.lon = float(atol(buf))/100000;
+  //Serial.print(s_msg.lon);
+  //Serial.print("-");
+  
+  c_msg.substring(31,35).toCharArray(buf, 13);
+  s_msg.hum = atoi(buf);
+  //Serial.print(s_msg.hum);
+  //Serial.print("-");
+  
+  c_msg.substring(35,39).toCharArray(buf, 13);
+  s_msg.tem = atoi(buf);
+  //Serial.print(s_msg.tem);
+  //Serial.print("-");
+  
+  c_msg.substring(39,47).toCharArray(buf, 13);
+  s_msg.no2 = atol(buf);
+  //Serial.print(s_msg.no2);
+  //Serial.print("-");
+  
+  c_msg.substring(47,55).toCharArray(buf, 13);
+  s_msg.co = atol(buf);
+  //Serial.println(s_msg.co);
+      
+  return s_msg; //Everything  OK
 }
