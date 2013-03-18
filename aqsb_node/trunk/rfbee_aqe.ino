cb3d022bc672a9f7d22c6ebaa24e462bd2d74849
DustinDemuth <DustinDemuth@9b2695e2-e135-4693-8c03-170fd80ef316>
2013-03-18 11:53:59 +0000

    Calculating Arithmetic of 15 Measuemrents. That leads to one transmission per minute
    git-svn-id: https://svn.52north.org/svn/swe/incubation/sensebox/AirQualitySenseBox@19571 9b2695e2-e135-4693-8c03-170fd80ef316

--- aqsb_node/trunk/rfbee_aqe.ino
+++ aqsb_node/trunk/rfbee_aqe.ino
@@ -18,9 +18,7 @@
 #define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
 #endif
 
-void (*softReset) (void) = 0; //declare reset function @ address 0
 volatile boolean f_wdt=1;
-
 // Watchdog Interrupt Service / is executed when  watchdog timed out
 ISR(WDT_vect) {
   f_wdt=1;  // set global flag
@@ -28,7 +26,7 @@ ISR(WDT_vect) {
 
 //****************************************************************
 // 0=16ms, 1=32ms,2=64ms,3=128ms,4=250ms,5=500ms
-// 6=1 sec,7=2 sec, 8=4 sec, 9= 8sec
+// 6=1 sec,7=2 sec, 8=4 sec, 9= 8secr
 void setup_watchdog(int ii) {
   byte bb;
   int ww;
@@ -49,7 +47,7 @@ void setup_watchdog(int ii) {
 // set system into sleep state
 // system wakes up when watchdog is timed out
 void system_sleep() {
-  setup_watchdog(9); //sleep for 8 Seconds....
+  setup_watchdog(8); //sleep for 4 Seconds....
   
   cbi(ADCSRA,ADEN);  // switch Analog to Digitalconverter OFF
   set_sleep_mode(SLEEP_MODE_PWR_DOWN); // sleep mode is set here
@@ -57,29 +55,30 @@ void system_sleep() {
   sleep_mode();                 // System sleeps here
   sleep_disable();              // System continues execution here when watchdog timed out (Or any other interrupt is called, i guess)
   sbi(ADCSRA,ADEN);             // switch Analog to Digitalconverter ON
-  
   wdt_disable();//disable watchdog
 }
 
 EggBus eggBus;
-
 //#define DHT22_NO_FLOAT //prevent floatingpoint-stuff
 DHT22 dht(5);
-
 //#define _GPS_NO_STATS
 TinyGPS gps;
-
-
 uint8_t mac[6]; //MAC Address of the AQ-Shield
-
 char msg[56]; //The message that is send away...
-unsigned long counter = 0;
-char ctr[11]; //This char array can hold the counter...
 
-#define BEEID 1 //0-254
-#define TARGETBEE 254
 
-//#define DEBUG
+#define BEEID 0 //0-254, Address of this RFBee, We akk have 0...
+#define TARGETBEE 254 // The Address of the RFBee which should receive the Message
+
+//Arithmetic Mean of NUMCYCLES measurements is calculated and send away....
+#define NUMCYCLES 15 //15 Cycles followed by a 4 Second break ~> 60seconds
+unsigned short cycles;
+
+unsigned long no2;
+unsigned long co;
+int t;
+int h;
+
 
 void setup(){
   //sleep
@@ -90,9 +89,6 @@ void setup(){
  
   RFBEE.init(BEEID); //Modified!
   Serial.begin(9600);
-  #ifdef DEBUG
-    Serial.println("ok");
-  #endif
 
   //Provide power... 
   //see:  http://www.seeedstudio.com/wiki/Grove_-_XBee_Carrier#Usage
@@ -112,10 +108,6 @@ void loop()
   wdt_enable(WDTO_4S); //enable Watchdog. The System has 4 Seconds to complete the loop. If this can't be achieved, the device restarts....
   if (f_wdt == 1) f_wdt = 0; 
    // do your job... 
-  #ifdef DEBUG
-    Serial.print("L.S: #");
-  #endif
-  
   
   //look if somebody send data, this might be necessary to free the input buffer, if not done device stops responding
   unsigned char rxData1[100];               // data len
@@ -130,59 +122,24 @@ void loop()
         result1=receiveData(rxData1, &len1, &srcAddress1, &destAddress1, (unsigned char *)&rssi1 , &lqi1);
     }
   free(rxData1); 
-  
-  //COUNTER
-  counter++;
-  //char ctr[11];
-  sprintf(ctr, "%010lu", counter);
-  RFBEE.sendDta(10,(unsigned char*)ctr, TARGETBEE);
-  #ifdef DEBUG 
-    Serial.print(counter);
-    Serial.print(" ms: ");
-    Serial.println(millis());
-    delay(100); //leave some time to recive data....
-  #endif
-  
-  #ifdef DEBUG 
-    Serial.println("GPS");
-    delay(100); //leave some time to recive data....
-  #endif
-  
+
   long lon=gps.GPS_INVALID_ANGLE, lat = gps.GPS_INVALID_ANGLE;
-  feedgps(100); //param is the time in ms how long the serial port shoul be read... 
+  feedgps(200); //param is the time in ms how long the serial port shoul be read... 
   gps.get_position(&lat, &lon);
   
-  #ifdef DEBUG 
-    Serial.println("EB.I");
-    delay(100); //leave some time to recive data....
-  #endif
-  
-  unsigned long no2;
-  unsigned long co;
   
   eggBus.init();
-
-  #ifdef DEBUG
-    Serial.println("EB.N");
-  #endif
   
   while(eggBus.next()){
 
-    #ifdef DEBUG
-      Serial.println("EB.W");
-    #endif
-    
     if (mac[1]+mac[2]+mac[3]+mac[4]+mac[5]+mac[0] == 0) addressToArray(mac, eggBus.getSensorAddress());
 
     uint8_t numSensors = eggBus.getNumSensors();
     
     for(uint8_t ii = 0; ii < numSensors; ii++){
-      #ifdef DEBUG
-        Serial.println("EB.RS");
-      #endif
-      
-      if (strncmp(eggBus.getSensorType(ii), "CO", 2) == 0) co = eggBus.getSensorValue(ii);
-      else if(strncmp(eggBus.getSensorType(ii), "NO2", 3) == 0) no2 = eggBus.getSensorValue(ii);
+
+      if (strncmp(eggBus.getSensorType(ii), "CO", 2) == 0) co += eggBus.getSensorValue(ii);
+      else if(strncmp(eggBus.getSensorType(ii), "NO2", 3) == 0) no2 += eggBus.getSensorValue(ii);
     }
   }
   
@@ -196,41 +153,34 @@ void loop()
    co = 12345678;
    no2 = 87654321;
 */
-  #ifdef DEBUG
-    Serial.println("DHT.read");
-    delay(100); //leave some time to recive data....
-  #endif
-  
+
   dht.readData();
-  short int h = dht.getHumidityInt();
-  short int t = dht.getTemperatureCInt();
+  h += dht.getHumidityInt();
+  t += dht.getTemperatureCInt();
 
-  /*
-   Message:
-   12          |1|    9    | 9       |4   |  4 | 8      | 8      |
-   MAC         |:|  LAT    | LON     |HUM |TEM | NO2    | CO     |
-   ------------|:|---------|---------|----|----|--------|--------|
-   */
 
-  //char msg[56];
-  //Serial.println("BuildMsg");
-  sprintf(msg, "%02X%02X%02X%02X%02X%02X:%09li%09li%04d%04d%08lu%08lu",mac[0],mac[1],mac[2],mac[3],mac[4],mac[5],lat,lon,h,t,no2, co);
+  if (cycles++ == NUMCYCLES){
+    t = t / cycles;
+    h = h / cycles;
+    no2 = no2 / cycles;
+    co = co / cycles;
+    
+    /*
+     Message:
+     12          |1|    9    | 9       |4   |  4 | 8      | 8      |  = 55
+     MAC         |:|  LAT    | LON     |HUM |TEM | NO2    | CO     |
+     ------------|:|---------|---------|----|----|--------|--------|
+     */
+    sprintf(msg, "%02X%02X%02X%02X%02X%02X:%09li%09li%04d%04d%08lu%08lu",mac[0],mac[1],mac[2],mac[3],mac[4],mac[5],lat,lon,h,t,no2, co);
 
-  #ifdef DEBUG
-    for (int i = 0; i < 55; i++)
-      Serial.write(msg[i]);
-
-  Serial.println();
-  #endif
-  
-  RFBEE.sendDta(55,(unsigned char*)msg, TARGETBEE);
-  
-  #ifdef DEBUG
-    Serial.print("L.E: ");
-    Serial.println(freeRam());
-    Serial.println();
-    delay(100); //Serial has to receive data BEFORE systm goes to sleep...
-  #endif
+    RFBEE.sendDta(55,(unsigned char*)msg, TARGETBEE);
+    //Reset
+    cycles = 0;
+    t = 0;
+    h = 0;
+    no2 = 0;
+    co = 0;
+  }
   
   wdt_disable(); //disable watchdog....
 
@@ -239,7 +189,7 @@ void loop()
 }
 
 void feedgps(unsigned int i){
-  for (unsigned long start = millis(); millis() - start < i;) //Parse NMEA sentences for 500ms
+  for (unsigned long start = millis(); millis() - start < i;) //Parse NMEA sentences for i ms
   {
     while (Serial.available())
     {
@@ -248,34 +198,17 @@ void feedgps(unsigned int i){
   }
 }
 
-/*
-void printAddress(uint8_t * address){
- for(uint8_t jj = 0; jj < 6; jj++){
- if(address[jj] < 16) Serial.print("0");
- Serial.print(address[jj], HEX);
- if(jj != 5 ) Serial.print(":");
- }
- Serial.println();
- }
- */
-
 void addressToArray(byte mac[], uint8_t * address){
   for(uint8_t jj = 0; jj < 6; jj++){
     mac[jj] = address[jj];
   }
 }
-
+/*
 int freeRam() {
   extern int __heap_start, *__brkval;
   int v;
   return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
 }
-
-/*********************************************************************************************************
- * END FILE
- *********************************************************************************************************/
-
-
-
+*/
 
 
