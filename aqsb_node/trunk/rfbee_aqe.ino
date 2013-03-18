4f9d320648094bdee7e5641af152599851b8576a
DustinDemuth <DustinDemuth@9b2695e2-e135-4693-8c03-170fd80ef316>
2013-03-18 11:53:40 +0000

    device is autoresetting now if it hangs....
    git-svn-id: https://svn.52north.org/svn/swe/incubation/sensebox/AirQualitySenseBox@19567 9b2695e2-e135-4693-8c03-170fd80ef316

--- aqsb_node/trunk/rfbee_aqe.ino
+++ aqsb_node/trunk/rfbee_aqe.ino
@@ -18,7 +18,7 @@
 #define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
 #endif
 
-
+void (*softReset) (void) = 0; //declare reset function @ address 0
 volatile boolean f_wdt=1;
 
 // Watchdog Interrupt Service / is executed when  watchdog timed out
@@ -46,15 +46,16 @@ void setup_watchdog(int ii) {
 }
 
 //****************************************************************  
-// set system into the sleep state
-// system wakes up when wtchdog is timed out
+// set system into sleep state
+// system wakes up when watchdog is timed out
 void system_sleep() {
   cbi(ADCSRA,ADEN);  // switch Analog to Digitalconverter OFF
   set_sleep_mode(SLEEP_MODE_PWR_DOWN); // sleep mode is set here
   sleep_enable();
-  sleep_mode();                        // System sleeps here
-  sleep_disable();              // System continues execution here when watchdog timed out
-  sbi(ADCSRA,ADEN);                    // switch Analog to Digitalconverter ON
+  sleep_mode();                 // System sleeps here
+  sleep_disable();              // System continues execution here when watchdog timed out (Or any other interrupt is called, i guess)
+  sbi(ADCSRA,ADEN);             // switch Analog to Digitalconverter ON
+  wdt_disable();//disable watchdog
 }
 
 EggBus eggBus;
@@ -66,82 +67,119 @@ DHT22 dht(5);
 TinyGPS gps;
 
 
-uint8_t mac[6];
+uint8_t mac[6]; //MAC Address of the AQ-Shield
+
 char msg[56]; //The message that is send away...
 unsigned long counter = 0;
 char ctr[11]; //This char array can hold the counter...
 
 #define BEEID 16 //0-255
 
+//#define DEBUG
+
 void setup(){
   //sleep
-
   cbi( SMCR,SE );      // sleep enable, power down mode
   cbi( SMCR,SM0 );     // power down mode
   sbi( SMCR,SM1 );     // power down mode
   cbi( SMCR,SM2 );     // power down mode
-  setup_watchdog(6);
-
+ 
   RFBEE.init(BEEID); //Modified!
   Serial.begin(9600);
-  Serial.println("ok");
+  #ifdef DEBUG
+    Serial.println("ok");
+  #endif
 
   //Provide power... 
   //see:  http://www.seeedstudio.com/wiki/Grove_-_XBee_Carrier#Usage
-  pinMode(16, OUTPUT);
-  digitalWrite(16, LOW);
-  pinMode(17, OUTPUT);
+  //pinMode(16, OUTPUT);
+  //digitalWrite(16, LOW);
+  pinMode(17, OUTPUT); // GPS
   digitalWrite(17, LOW);
+
 }
 
 void loop()
 {
-  //Sleep
-  if (f_wdt==1) // wait for timed out watchdog
-    f_wdt=0;   
-
+  
+  wdt_enable(WDTO_4S); //enable Watchdog. The System has 4 Seconds to complete the loop. If this can't be achieved, the device restarts....
+  if (f_wdt == 1) f_wdt = 0; 
+ 
   // do your job... 
-  Serial.print("L.S: #");
+  #ifdef DEBUG
+    Serial.print("L.S: #");
+  #endif
   
   //COUNTER
   counter++;
   //char ctr[11];
   sprintf(ctr, "%010lu", counter);
   RFBEE.sendDta(10,(unsigned char*)ctr);
-  Serial.print(counter);
-  Serial.print(" ms: ");
-  Serial.println(millis());
-  delay(100); //leave some time to recive data....
+  #ifdef DEBUG 
+    Serial.print(counter);
+    Serial.print(" ms: ");
+    Serial.println(millis());
+    delay(100); //leave some time to recive data....
+  #endif
   
-  Serial.println("GPS");
+  #ifdef DEBUG 
+    Serial.println("GPS");
+    delay(100); //leave some time to recive data....
+  #endif
+  
   long lon=gps.GPS_INVALID_ANGLE, lat = gps.GPS_INVALID_ANGLE;
   feedgps(100); //param is the time in ms how long the serial port shoul be read... 
   gps.get_position(&lat, &lon);
   
-  delay(100); //leave some time to recive data....
-
-  Serial.println("EB.I");
-  delay(100); //leave some time to recive data....
-  eggBus.init();
+  #ifdef DEBUG 
+    Serial.println("EB.I");
+    delay(100); //leave some time to recive data....
+  #endif
+  
   unsigned long no2;
   unsigned long co;
+  
+  eggBus.init();
 
-  Serial.println("EB.N");
+  #ifdef DEBUG
+    Serial.println("EB.N");
+  #endif
+  
   while(eggBus.next()){
 
-    Serial.println("EB.W");
+    #ifdef DEBUG
+      Serial.println("EB.W");
+    #endif
+    
     if (mac[1]+mac[2]+mac[3]+mac[4]+mac[5]+mac[0] == 0) addressToArray(mac, eggBus.getSensorAddress());
 
     uint8_t numSensors = eggBus.getNumSensors();
+    
     for(uint8_t ii = 0; ii < numSensors; ii++){
-      Serial.println("EB.RS");
+      #ifdef DEBUG
+        Serial.println("EB.RS");
+      #endif
+      
       if (strncmp(eggBus.getSensorType(ii), "CO", 2) == 0) co = eggBus.getSensorValue(ii);
       else if(strncmp(eggBus.getSensorType(ii), "NO2", 3) == 0) no2 = eggBus.getSensorValue(ii);
     }
   }
-
-  Serial.println("DHT.read");
-  delay(100); //leave some time to recive data....
+  
+   /* 
+   mac[0] = 00;
+   mac[1] = 11;
+   mac[2] = 22;
+   mac[3] = 33;
+   mac[4] = 44;
+   mac[5] = 55;
+   co = 12345678;
+   no2 = 87654321;
+*/
+  #ifdef DEBUG
+    Serial.println("DHT.read");
+    delay(100); //leave some time to recive data....
+  #endif
+  
   dht.readData();
   short int h = dht.getHumidityInt();
   short int t = dht.getTemperatureCInt();
@@ -157,23 +195,28 @@ void loop()
   //Serial.println("BuildMsg");
   sprintf(msg, "%02X%02X%02X%02X%02X%02X:%09li%09li%04d%04d%08lu%08lu",mac[0],mac[1],mac[2],mac[3],mac[4],mac[5],lat,lon,h,t,no2, co);
 
-
-  for (int i = 0; i < 55; i++)
-    Serial.write(msg[i]);
+  #ifdef DEBUG
+    for (int i = 0; i < 55; i++)
+      Serial.write(msg[i]);
 
   Serial.println();
-
+  #endif
+  
   RFBEE.sendDta(55,(unsigned char*)msg);
+  
+  #ifdef DEBUG
+    Serial.print("L.E: ");
+    Serial.println(freeRam());
+    Serial.println();
+    delay(100); //Serial has to receive data BEFORE systm goes to sleep...
+  #endif
+  
+  wdt_disable(); //disable watchdog....
 
-  Serial.print("L.E: ");
-  Serial.println(freeRam());
-  Serial.println();
-  
-  
-  delay(100); //Serial has to receive data BEFORE systm goes to sleep...
+  //Now we need a second watchdog to wake up the device from sleep...
+  setup_watchdog(6);
   system_sleep(); // when we wake up, we’ll return to the top of the loop
 
-  //delay(1000);
 }
 
 void feedgps(unsigned int i){
